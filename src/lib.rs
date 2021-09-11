fc3acf2f7ab6e354abb128854b6bd40583463d56
xiurui-pan <panxiurui@outlook.com>
2021-09-11 11:38:45 +0800

    finished hw3

--- src/lib.rs
+++ src/lib.rs
@@ -10,12 +10,56 @@ pub mod console;
 mod syscall;
 mod lang_items;
 
+pub const STDOUT: usize = 1;
+
 use syscall::*;
 
 pub fn write(fd: usize, buf: &[u8]) -> isize { sys_write(fd, buf) }
+
 pub fn exit(exit_code: i32) -> isize { sys_exit(exit_code) }
+
 pub fn yield_() -> isize { sys_yield() }
-pub fn get_time() -> isize { sys_get_time() }
+
+pub fn get_time() -> isize {
+    let time = TimeVal::new();
+    match sys_get_time(&time, 0) {
+        0 => ((time.sec & 0xffff) * 1000 + time.usec / 1000) as isize,
+        _ => -1,
+    }
+}
+
+pub fn sleep(period_ms: usize) {
+    let start = get_time();
+    while get_time() < start + period_ms as isize {
+        sys_yield();
+    }
+}
+
+pub fn set_priority(prio: isize) -> isize {
+    sys_set_priority(prio)
+}
+
+pub fn wait(exit_code: &mut i32) -> isize {
+    loop {
+        match sys_waitpid(-1, exit_code as *mut _) {
+            -2 => { sys_yield(); }
+            n => { return n ; }
+        }
+    }
+}
+
+#[repr(C)]
+#[derive(Debug)]
+pub struct TimeVal {
+    pub sec: usize,
+    pub usec: usize,
+}
+
+impl TimeVal {
+    pub fn new() -> Self {
+        TimeVal { sec: 0, usec: 0 }
+    }
+}
 
 #[no_mangle]
 #[link_section = ".text.entry"]
--- src/syscall.rs
+++ src/syscall.rs
@@ -1,9 +1,11 @@
-// user/src/syscall.rs
+use super::TimeVal;
 
 const SYSCALL_WRITE: usize = 64;
 const SYSCALL_EXIT : usize = 93;
 const SYSCALL_YIELD : usize = 124;
+const SYSCALL_SET_PRIORITY: usize = 140;
 const SYSCALL_GET_TIME: usize = 169;
+const SYSCALL_WAITPID: usize = 260;
 
 fn syscall(id: usize, args: [usize; 3]) -> isize {
     let mut ret: isize;
@@ -28,4 +30,14 @@ pub fn sys_exit(xstate: i32) -> isize {
 
 pub fn sys_yield() -> isize { syscall(SYSCALL_YIELD, [0, 0, 0])}
 
-pub fn sys_get_time() -> isize { syscall(SYSCALL_GET_TIME, [0, 0,0 ])}
+pub fn sys_get_time(time: &TimeVal, tz: usize) -> isize {
+    syscall(SYSCALL_GET_TIME, [time as *const _ as usize, tz, 0])
+}
+
+pub fn sys_waitpid(pid: isize, xstatus: *mut i32) -> isize {
+    syscall(SYSCALL_WAITPID, [pid as usize, xstatus as usize, 0])
+}
+
+pub fn sys_set_priority(prio: isize) -> isize {
+    syscall(SYSCALL_SET_PRIORITY, [prio as usize, 0, 0])
+}
